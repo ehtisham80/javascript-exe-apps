90e80dbe95b3ef030010bfc5c8ad366eab033db9
Ehtisham Zaheer <65091011+ehtisham80@users.noreply.github.com>
2022-01-09 19:44:22 +0100

    callbackTimes

---
+++ callbackTimes/app.js
@@ -0,0 +1,7 @@
+const times = (n, fn, context = undefined) => {
+  let i = 0;
+  while (fn.call(context, i) !== false && ++i < n) {}
+};
+var output = "";
+times(5, (i) => (output += i));
+console.log(output); // 01234
