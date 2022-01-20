c862bc2a339608d728eeca72224c5bdaf533e826
Ehtisham Zaheer <65091011+ehtisham80@users.noreply.github.com>
2022-01-20 14:58:36 +0100

    CommaSeparatedValues

---
+++ CommaSeparatedValues/app.js
@@ -0,0 +1,18 @@
+
+const arrayToCSV = (arr, delimiter = ",") =>
+  arr.map((v) => v.map((x) => `"${x}"`).join(delimiter)).join("\n");
+console.log(
+  arrayToCSV([
+    ["a", "b"],
+    ["c", "d"],
+  ])
+);
+console.log(
+  arrayToCSV(
+    [
+      ["a", "b"],
+      ["c", "d"],
+    ],
+    ";"
+  )
+);
