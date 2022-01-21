d9f2e6c6e4533c6f425bd505851a8687904ebdb9
Ehtisham Zaheer <65091011+ehtisham80@users.noreply.github.com>
2022-01-21 09:55:48 +0100

    computeSumTwo

---
+++ computeSumTwo/app.js
@@ -0,0 +1,10 @@
+function computeSumTwo(arr) {
+  var result = [0, 0];
+  for (var i = 0; i < arr.length; i++) {
+    if (i % 2) result[1] += arr[i];
+    else result[0] += arr[i];
+  }
+  return result;
+}
+
+console.log(computeSumTwo([1, 3, 6, 2, 5, 10]));
