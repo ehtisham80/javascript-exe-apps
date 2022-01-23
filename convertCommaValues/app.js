697a03ec0e005b99e62a2a43d10d98c879aba2e6
Ehtisham Zaheer <65091011+ehtisham80@users.noreply.github.com>
2022-01-23 11:20:03 +0100

    ConvertCommaValues

---
+++ convertCommaValues/app.js
@@ -0,0 +1,15 @@
+const convertCommaValues = (data, delimiter = ",") => {
+  const titles = data.slice(0, data.indexOf("\n")).split(delimiter);
+  return data
+    .slice(data.indexOf("\n") + 1)
+    .split("\n")
+    .map((v) => {
+      const values = v.split(delimiter);
+      return titles.reduce(
+        (obj, title, index) => ((obj[title] = values[index]), obj),
+        {}
+      );
+    });
+};
+console.log(convertCommaValues("col1,col2\na,b\nc,d")); // [{'col1': 'a', 'col2': 'b'}, {'col1': 'c', 'col2': 'd'}];
+console.log(convertCommaValues("col1;col2\na;b\nc;d", ";")); // [{'col1': 'a', 'col2': 'b'}, {'col1': 'c', 'col2': 'd'}];
