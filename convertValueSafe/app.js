81b744c7161ff096b93e10e5dbf4e8c2831450c2
Ehtisham Zaheer <65091011+ehtisham80@users.noreply.github.com>
2022-01-26 16:10:02 +0100

    convert value safe

---
+++ convertValueSafe/app.js
@@ -0,0 +1,5 @@
+const toSafeInteger = num =>
+
+    Math.round( Math.max( Math.min( num, Number.MAX_SAFE_INTEGER ), Number.MIN_SAFE_INTEGER ) );
+    console.log( toSafeInteger( '3.2' ) );
+    console.log( toSafeInteger( Infinity ) );
