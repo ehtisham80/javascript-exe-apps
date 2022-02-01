0356d96e9665b169134c39daacc00faceb5a1729
Ehtisham Zaheer <65091011+ehtisham80@users.noreply.github.com>
2022-02-01 23:24:32 +0100

    divisibleNums

---
+++ divisibleNums/app.js
@@ -0,0 +1,6 @@
+const isDivisibleNums = ( dividend, divisor ) => dividend % divisor === 0;
+
+console.log( isDivisibleNums( 6, 3 ) );
+console.log( isDivisibleNums( 5, 3 ) );
+console.log( isDivisibleNums( 100, 10 ) );
+console.log(isDivisibleNums(0, 3));
