ff981d0994d50f9092bf6255bbccd0e1e2b6bc0a
Ehtisham Zaheer <65091011+ehtisham80@users.noreply.github.com>
2022-02-04 12:15:57 +0100

    ElementString

---
+++ ElementString/app.js
@@ -0,0 +1,13 @@
+
+const createElement = (str) => {
+  const el = document.createElement("div");
+  el.innerHTML = str;
+  return el.firstElementChild;
+};
+
+const el = createElement(
+  `<div class="container">
+    <p>Hello!</p>
+  </div>`
+);
+console.log(el.className); // 'container'
