9b5bb40fc5bcba1703096407317b5663bf62609c
Ehtisham Zaheer <65091011+ehtisham80@users.noreply.github.com>
2022-02-12 22:51:59 +0100

    filterUnspecifiedNums

---
+++ filterUnspecifiedNums/app.js
@@ -0,0 +1,21 @@
+const reducedFilterNums = (data, keys, fn) =>
+  data.filter(fn).map((el) =>
+    keys.reduce((acc, key) => {
+      acc[key] = el[key];
+      return acc;
+    }, {})
+  );
+const data = [
+  {
+    id: 1,
+    name: "john",
+    age: 24,
+  },
+  {
+    id: 2,
+    name: "mike",
+    age: 50,
+  },
+];
+
+console.log(reducedFilterNums(data, ["id", "name"], (item) => item.age > 24));
