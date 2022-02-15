76d663d78c4f202814a09e26bfa78f89e7f8788f
Ehtisham Zaheer <65091011+ehtisham80@users.noreply.github.com>
2022-02-15 20:56:26 +0100

    Findkey

---
+++ FindKey/app.js
@@ -0,0 +1,14 @@
+
+const findKey = (obj, fn) =>
+  Object.keys(obj).find((key) => fn(obj[key], key, obj));
+
+console.log(
+  findKey(
+    {
+      barney: { age: 36, active: true },
+      fred: { age: 40, active: false },
+      pebbles: { age: 1, active: true },
+    },
+    (o) => o["active"]
+  )
+);
