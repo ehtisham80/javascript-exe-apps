9bf7a1fa5379fcdb4a7687e178978e03f359472b
Ehtisham Zaheer <65091011+ehtisham80@users.noreply.github.com>
2022-02-19 09:52:14 +0100

    findNumberMode

---
+++ findNumberMode/app.js
@@ -0,0 +1,16 @@
+function findNumerMode(arr) {
+  const ctr = [];
+  let ans = 0;
+
+  for (var i = 0; i < 10; i++) {
+    ctr.push(0);
+  }
+  for (var i = 0; i < arr.length; i++) {
+    ctr[arr[i] - 1]++;
+    if (ctr[arr[i] - 1] > ctr[ans]) {
+      ans = arr[i] - 1;
+    }
+  }
+  return ans + 1;
+}
+console.log(findNumerMode([1, 2, 3, 2, 2, 8, 1, 9]));
