eaa76031e9cf1660bb74a5ee102b7ef9ed77f7ef
Ehtisham Zaheer <65091011+ehtisham80@users.noreply.github.com>
2022-02-27 19:04:26 +0100

    floatpointDecimal

---
+++ floatpointDecimal/app.js
@@ -0,0 +1,4 @@
+const floatpointDecimalMark = (num) => num.toLocaleString("en-US");
+console.log(floatpointDecimalMark(12305030388.9087));
+console.log(floatpointDecimalMark(123.2264));
+console.log(floatpointDecimalMark(-100.1));
