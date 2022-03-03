2a95b9a74e002e1b8fba6e92eaf7353abfdc5e1a
Ehtisham Zaheer <65091011+ehtisham80@users.noreply.github.com>
2022-03-03 20:08:16 +0100

    generatePermutations

---
+++ generatePermutations/app.js
@@ -0,0 +1,17 @@
+const generateStringPermutations = (str) => {
+  if (str.length <= 2) return str.length === 2 ? [str, str[1] + str[0]] : [str];
+  return str
+    .split("")
+    .reduce(
+      (acc, letter, i) =>
+        acc.concat(
+          generateStringPermutations(str.slice(0, i) + str.slice(i + 1)).map(
+            (val) => letter + val
+          )
+        ),
+      []
+    );
+};
+
+console.log(generateStringPermutations("abc"));
+console.log(generateStringPermutations("*$*"));
