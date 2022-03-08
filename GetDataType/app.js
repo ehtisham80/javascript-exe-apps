55e0ef6f406ac77792b3dbd24bd516d1494b5e1a
Ehtisham Zaheer <65091011+ehtisham80@users.noreply.github.com>
2022-03-08 21:37:37 +0100

    GetDataTypes

---
+++ GetDataType/app.js
@@ -0,0 +1,18 @@
+function DetectDataType(value) {
+  var dtypes = [Function, RegExp, Number, String, Boolean, Object],
+    x,
+    len;
+
+  if (typeof value === "object" || typeof value === "function") {
+    for (x = 0, len = dtypes.length; x < len; x++) {
+      if (value instanceof dtypes[x]) {
+        return dtypes[x];
+      }
+    }
+  }
+
+  return typeof value;
+}
+console.log(DetectDataType(12));
+console.log(DetectDataType("learn code"));
+console.log(DetectDataType(false));
