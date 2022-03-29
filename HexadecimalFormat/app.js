e7f05993678931ff7d2f39ddb15f3804d6c243cd
Ehtisham Zaheer <65091011+ehtisham80@users.noreply.github.com>
2022-03-29 20:27:54 +0200

    HexadecimalFormat

---
+++ HexadecimalFormat/app.js
@@ -0,0 +1,11 @@
+function HexadecimalFormat(str) {
+  var arr1 = [];
+  for (var n = 0, l = str.length; n < l; n++) {
+    var hex = Number(str.charCodeAt(n)).toString(16);
+    arr1.push(hex);
+  }
+  return arr1.join("");
+}
+
+console.log(HexadecimalFormat("12"));
+console.log(HexadecimalFormat("100"));
